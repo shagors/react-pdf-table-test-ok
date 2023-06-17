39053bf961eac0427af15db3fe1fa4f7c779263a
thtshajjad <shagors@users.noreply.github.com>
2023-06-17 11:37:04 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import "./App.css";
+import { PDFViewer } from "@react-pdf/renderer";
+import Invoice from "./components/Invoice";
+import { invoice } from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <PDFViewer width="1000" height="600" className="app">
+        <Invoice invoice={invoice} />
+      </PDFViewer>
+    </Fragment>
   );
 }
 
